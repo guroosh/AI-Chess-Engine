849b15c26cc9db6cb4c19623a3a444c0e380d849
guroosh <chaudhary14032@iiitd.ac.in>
2017-10-02 22:14:36 +0530

    functions added for checking path and destination validity

--- Bishop.py
+++ Bishop.py
@@ -1,6 +1,52 @@
 from Piece import Piece
 
 
+def isPathObstructed(board, fromX, fromY, toX, toY):
+    goRight = False
+    goBelow = False
+    path_len = abs(toX - fromX - 1)
+
+    if fromX < toX:
+        goBelow = True
+    if fromY < toY:
+        goRight = True
+
+    if goBelow and goRight:
+        for j in range(path_len):
+            i = j + 1
+            if board.isSpotVacant(fromX + i, fromY + i):
+                return False
+        return True
+    elif goBelow and not goRight:
+        for j in range(path_len):
+            i = j + 1
+            if board.isSpotVacant(fromX + i, fromY - i):
+                return False
+        return True
+    elif not goBelow and goRight:
+        for j in range(path_len):
+            i = j + 1
+            if board.isSpotVacant(fromX - i, fromY + i):
+                return False
+        return True
+    else:
+        for j in range(path_len):
+            i = j + 1
+            if board.isSpotVacant(fromX - i, fromY - i):
+                return False
+        return True
+
+
+def isDestinationEmpty(board, fromX, fromY, toX, toY):
+    piece_name = board.getPiece(fromX, fromY)
+    piece_name1 = board.getPiece(toX, toY)
+    if piece_name1[0] == '.':
+        return True
+    if piece_name[0] is not piece_name1[0]:
+        return True
+    return False
+
+
 class Bishop(Piece):
     def __init__(self, available, x, y):
         super().__init__(available, x, y)
@@ -9,5 +55,6 @@ class Bishop(Piece):
         if not super().isValid(board, fromX, fromY, toX, toY):
             return False
         if abs(fromX - toX) == abs(fromY - toY):
-            return True
+            if (not isPathObstructed(board, fromX, fromY, toX, toY)) and isDestinationEmpty(board, fromX, fromY, toX, toY):
+                return True
         return False
